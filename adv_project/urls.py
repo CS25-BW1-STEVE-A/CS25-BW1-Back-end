cf507141f2d6b2b8995f71d041ec19ff35c1e11d
ampersand <hossainazifa@gmail.com>
2020-02-04 14:10:35 -0800

    urls
    Co-authored-by: Heorhii Siburov <g.siburov@gmail.com>

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -19,9 +19,13 @@ from django.conf.urls import url
 
 from rest_framework.authtoken import views
 
+from adventure import api
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^login/', views.obtain_auth_token),
     path('', include('rest_auth.urls')),
     path('registration/', include('rest_auth.registration.urls')),
+    path('adv/init', api.initialize),
+    path('adv/move', api.move),
 ]
