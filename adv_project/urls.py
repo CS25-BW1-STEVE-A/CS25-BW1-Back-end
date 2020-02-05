df33d6b1bacf48bdb6b6f2b08f3362307c5cd4aa
ampersand <hossainazifa@gmail.com>
2020-02-05 15:50:35 -0800

    csrf token

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls import url
 from rest_framework.authtoken import views
 
 from adventure import api
+from adventure import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +30,6 @@ urlpatterns = [
     path('registration/', include('rest_auth.registration.urls')),
     path('adv/init', api.initialize),
     path('adv/move', api.move),
+    path('csrf/', views.csrf),
+    path('ping/', views.ping),
 ]
