2b40adf2ac7d52e41bcf20c9e5da9529af347269
ampersand <hossainazifa@gmail.com>
2020-02-07 08:17:08 -0800

    scheme

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -153,6 +153,6 @@ REST_FRAMEWORK = {
 # CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ORIGIN_WHITELIST = ['http://localhost:3000', 'chickenrunner.netlify.com']
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000', 'https://chickenrunner.netlify.com']
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000',  'chickenrunner.netlify.com']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000',  'https://chickenrunner.netlify.com']
